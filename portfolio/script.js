2d5d24e58f228ade7cb23409c57da414f588bac6
Hanpiranpm <annromania@gmail.com>
2022-02-01 00:16:14 +0300

    feat: add internationalization to portfolio

--- portfolio/script.js
+++ portfolio/script.js
@@ -20,8 +20,8 @@ hamburger.addEventListener("click", toggleMenu);
 navLinks.forEach((el) => el.addEventListener("click", closeMenu));
 
 // Portfolio section - changing season button 
+const portfolioBtn = document.querySelectorAll('.period-btn');
 const portfolioBtns = document.querySelector('.portfolio-btns');
-const portfolioBtn = document.querySelector('.period-btn');
 const portfolioImages = document.querySelectorAll('.portfolio-image');
 const seasons = ['winter', 'spring', 'summer', 'autumn'];
 
@@ -34,6 +34,13 @@ const preloadImages = (el) => {
 
 seasons.forEach(preloadImages);
 
+const portfolioBtnsClick = (event) => {
+  if (event.target.classList.contains('period-btn')) {
+    changeImage(event);
+    changeActive(event);
+  };
+}
+
 const changeImage = (event) => /* change images and season */
 portfolioImages.forEach ((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index+1}.jpg`);
 
@@ -42,12 +49,63 @@ const changeActive = (event) => /* change button from active to inactive and vic
   event.target.classList.add('active');
 }
 
-const portfolioBtnsClick = (event) => /*  */ {
-  if (event.target.classList.contains('period-btn')) {
-    changeImage(event);
-    changeActive(event);
-  };
-}
-
 portfolioBtns.addEventListener("click", portfolioBtnsClick);
+
+/* Buttons*/
+
+const animateButton = (e) => {
+  e.preventDefault;
+  e.target.classList.remove("animate");
+
+  e.target.classList.add("animate");
+  setTimeout(() => {
+    e.target.classList.remove("animate");
+  }, 700);
+};
+
+const diagButtons = document.querySelectorAll(".btn-diagonal");
+
+diagButtons.forEach((el) =>
+  el.addEventListener("click", animateButton, false)
+);
+
+/* Internationalization */
+
+import i18Obj from "./translate.js";
+
+const switchLng = document.querySelector(".lng");
+const switchLngBtns = document.querySelectorAll(".lng-btn");
+
+const onLngBtnClick = (event) => {
+  if (event.target.classList.contains("lng-btn")) {
+    state.lang = event.target.dataset.lng;
+    updateClasses();
+  }
+};
+
+const updateClasses = () => {
+  changeClassLngActive();
+  getTranslate(state.lang);
+};
+
+const changeClassLngActive = () => {
+  switchLngBtns.forEach((el) => el.classList.remove("lng-active"));
+  const activeBtn = document.querySelector(`[data-lng=${state.lang}]`);
+  activeBtn.classList.add("lng-active");
+};
+
+const getTranslate = (currentLanguage) => {
+  const dataElements = document.querySelectorAll("[data-i18]");
+
+  dataElements.forEach((el) => {
+    if (el.placeholder) {
+      el.placeholder = i18Obj[currentLanguage][el.dataset.i18];
+      el.textContent = "";
+    } else {
+      el.textContent = i18Obj[currentLanguage][el.dataset.i18];
+    }
+  });
+};
+
+switchLng.addEventListener("click", onLngBtnClick);
 
