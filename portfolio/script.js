d13bd4fd4fc21bc2cac655e002a4ca662f32110c
Hanpiranpm <annromania@gmail.com>
2022-01-31 15:17:54 +0300

    feat: add switching images in portfolio section

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,5 @@
+let state = { lang: "en", theme: "dark" }; /* Autochoise: you can switch to russian lang or choose light theme*/
+
 // Hamburger
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
@@ -18,3 +20,34 @@ hamburger.addEventListener("click", toggleMenu);
 navLinks.forEach((el) => el.addEventListener("click", closeMenu));
 
 // Portfolio section - changing season button 
+const portfolioBtns = document.querySelector('.portfolio-btns');
+const portfolioBtn = document.querySelector('.period-btn');
+const portfolioImages = document.querySelectorAll('.portfolio-image');
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
+const preloadImages = (el) => {
+  for (let i = 1; i <= 6; i++) {
+    const img = new Image();
+    img.src = `./assets/img/${el}/${i}.jpg`;
+  }
+};
+
+seasons.forEach(preloadImages);
+
+const changeImage = (event) => /* change images and season */
+portfolioImages.forEach ((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index+1}.jpg`);
+
+const changeActive = (event) => /* change button from active to inactive and vice versa */ {
+  portfolioBtns.forEach((el) => el.classList.remove('active'));
+  event.target.classList.add('active');
+}
+
+const portfolioBtnsClick = (event) => /*  */ {
+  if (event.target.classList.contains('period-btn')) {
+    changeImage(event);
+    changeActive(event);
+  };
+}
+
+portfolioBtns.addEventListener("click", portfolioBtnsClick);
+
