9ea4393b232bf622c840fa909ab0e64d60e62c3e
Hanpiranpm <annromania@gmail.com>
2022-02-01 02:58:41 +0300

    feat: message console.log

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,23 +1,23 @@
-let firstVisit = { lang: "en", theme: "dark" }; /* Autochoise: you can switch to russian lang or choose light theme*/
+let firstVisit = { lang: 'en', theme: 'light' }; /* Autochoise: you can switch to russian lang or choose light theme*/
 
 // Hamburger
-const hamburger = document.querySelector(".hamburger");
-const navList = document.querySelector(".nav-list");
-const navLinks = document.querySelectorAll(".nav-link");
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+const navLinks = document.querySelectorAll('.nav-link');
 
 const toggleMenu = () => {
-  hamburger.classList.toggle("open");
-  navList.classList.toggle("open");
+  hamburger.classList.toggle('open');
+  navList.classList.toggle('open');
 };
 
 const closeMenu = () => {
-  hamburger.classList.remove("open");
-  navList.classList.remove("open");
+  hamburger.classList.remove('open');
+  navList.classList.remove('open');
 };
 
-hamburger.addEventListener("click", toggleMenu);
+hamburger.addEventListener('click', toggleMenu);
 
-navLinks.forEach((el) => el.addEventListener("click", closeMenu));
+navLinks.forEach((el) => el.addEventListener('click', closeMenu));
 
 // Portfolio section - changing season button 
 const portfolioBtn = document.querySelectorAll('.period-btn');
@@ -49,35 +49,35 @@ const changeActive = (event) => /* change button from active to inactive and vic
   event.target.classList.add('active');
 }
 
-portfolioBtns.addEventListener("click", portfolioBtnsClick);
+portfolioBtns.addEventListener('click', portfolioBtnsClick);
 
 /* Buttons*/
 
 const animateButton = (e) => {
   e.preventDefault;
-  e.target.classList.remove("animate");
+  e.target.classList.remove('animate');
 
-  e.target.classList.add("animate");
+  e.target.classList.add('animate');
   setTimeout(() => {
-    e.target.classList.remove("animate");
+    e.target.classList.remove('animate');
   }, 700);
 };
 
-const diagButtons = document.querySelectorAll(".btn-diagonal");
+const diagButtons = document.querySelectorAll('.btn-diagonal');
 
 diagButtons.forEach((el) =>
-  el.addEventListener("click", animateButton, false)
+  el.addEventListener('click', animateButton, false)
 );
 
 /* Internationalization */
 
-import i18Obj from "./translate.js";
+import i18Obj from './translate.js';
 
-const switchLng = document.querySelector(".lng");
-const switchLngBtns = document.querySelectorAll(".lng-btn");
+const switchLng = document.querySelector('.lng');
+const switchLngBtns = document.querySelectorAll('.lng-btn');
 
 const lngBtnClick = (event) => {
-  if (event.target.classList.contains("lng-btn")) {
+  if (event.target.classList.contains('lng-btn')) {
     firstVisit.lang = event.target.dataset.lng;
     translateClasses();
   }
@@ -89,58 +89,58 @@ const translateClasses = () => {
 };
 
 const changeClassLngActive = () => {
-  switchLngBtns.forEach((el) => el.classList.remove("lng-active"));
+  switchLngBtns.forEach((el) => el.classList.remove('lng-active'));
   const activeBtn = document.querySelector(`[data-lng=${firstVisit.lang}]`);
-  activeBtn.classList.add("lng-active");
+  activeBtn.classList.add('lng-active');
 };
 
 const getTranslate = (currentLanguage) => {
-  const dataElements = document.querySelectorAll("[data-i18]");
+  const dataElements = document.querySelectorAll('[data-i18]');
 
   dataElements.forEach((el) => {
     if (el.placeholder) {
       el.placeholder = i18Obj[currentLanguage][el.dataset.i18];
-      el.textContent = "";
+      el.textContent = '';
     } else {
       el.textContent = i18Obj[currentLanguage][el.dataset.i18];
     }
   });
 };
 
-switchLng.addEventListener("click", lngBtnClick);
+switchLng.addEventListener('click', lngBtnClick);
 
 
 /* Switching themes */
 
-const themeBtn = document.querySelector(".theme-btn");
-const sectionTitle = document.querySelectorAll(".section-title");
-const text = document.querySelectorAll(".text-el");
-const openSidebar = document.querySelectorAll(".sidebar");
+const themeBtn = document.querySelector('.theme-btn');
+const sectionTitle = document.querySelectorAll('.section-title');
+const text = document.querySelectorAll('.text-el');
+const openSidebar = document.querySelectorAll('.sidebar');
 
 const ThemeBtnClick = () => {
-  firstVisit.theme = firstVisit.theme === "light" ? "dark" : "light";
+  firstVisit.theme = firstVisit.theme === 'light' ? 'dark' : 'light';
   switchTheme();
 };
 
 const switchTheme = () => {
   if (
-    firstVisit.theme === "light" &&
-    !themeBtn.classList.contains("light-theme-btn")
+    firstVisit.theme === 'light' &&
+    !themeBtn.classList.contains('light-theme-btn')
   ) {
-    themeBtn.classList.add("light-theme-btn");
-    text.forEach((el) => el.classList.add("light-theme"));
-    openSidebar.forEach((el) => el.classList.add("light-theme-sidebar"));
+    themeBtn.classList.add('light-theme-btn');
+    text.forEach((el) => el.classList.add('light-theme'));
+    openSidebar.forEach((el) => el.classList.add('light-theme-sidebar'));
   } else if (
-    firstVisit.theme === "dark" &&
-    themeBtn.classList.contains("light-theme-btn")
+    firstVisit.theme === 'dark' &&
+    themeBtn.classList.contains('light-theme-btn')
   ) {
-    themeBtn.classList.remove("light-theme-btn");
-    text.forEach((el) => el.classList.remove("light-theme"));
-    openSidebar.forEach((el) => el.classList.remove("light-theme-sidebar"));
+    themeBtn.classList.remove('light-theme-btn');
+    text.forEach((el) => el.classList.remove('light-theme'));
+    openSidebar.forEach((el) => el.classList.remove('light-theme-sidebar'));
   }
 };
 
-themeBtn.addEventListener("click", ThemeBtnClick);
+themeBtn.addEventListener('click', ThemeBtnClick);
 
 /* Local storage*/
 
