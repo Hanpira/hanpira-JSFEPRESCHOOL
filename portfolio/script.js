eb5a0dbafffb9fc9fdf5f0036af1ab70db95da86
Hanpiranpm <annromania@gmail.com>
2022-02-06 14:28:34 +0300

    fix: changing themes

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,4 +1,4 @@
-let firstVisit = { lang: 'en', theme: 'light' }; /* Autochoise: you can switch to russian lang or choose light theme*/
+let firstVisit = { lang: 'en', theme: 'dark' }; /* Autochoice: you can switch to russian lang or choose light theme*/
 
 // Hamburger
 const hamburger = document.querySelector('.hamburger');
